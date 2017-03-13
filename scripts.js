3c6e032a065dd3d5b5b3798f78331865e3cc4800
Emily Oliver <mleeoliver@gmail.com>
2017-03-13 19:10:40 +0000

    Move javascript to scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var expandedMenu = document.getElementById("expanded-menu");
+
+function hide(element) {
+	element.className -= "hidden"
+};
+
+function show(element) {
+	element.className += "hidden"
+};
+		
+function btnclick() {
+	if (expandedMenu.className.includes("hidden")) show(expandedMenu);
+	else hide(expandedMenu);
+};
+		
+
+document.getElementById("hamburger").onclick = btnclick();
