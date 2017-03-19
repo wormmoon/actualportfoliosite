1f052e092fa2134e93336b5f28ace65ae9c5d416
Emily Oliver <mleeoliver@gmail.com>
2017-03-19 11:41:46 +0000

    Generalise btnclick function and add console.logs

--- scripts.js
+++ scripts.js
@@ -3,20 +3,25 @@ window.onload = function() {
 	var expandedMenu = document.getElementById("expanded-menu");
 
 	function hide(element) {
-		element.className -= "hidden";
+		element.className += "hidden";
+		console.log("hide run")
 	}
 
 	function show(element) {
-		element.className += "hidden";
+		element.className -= "hidden";
+		console.log("show run")
 	}
 			
-	function btnclick() {
-		if (expandedMenu.className.includes("hidden")) {
-			show(expandedMenu);
+	function btnclick(element) {
+		if (element.className.includes("hidden")) {
+			show(element);
 		} else {
-			hide(expandedMenu);}
+			hide(element);
+		}
+
+		console.log("btn click run")
 	}	
 
-	document.getElementById("hamburger").onclick = btnclick();
+	document.getElementById("hamburger").onclick = btnclick(expandedMenu);
+};
 
-}
